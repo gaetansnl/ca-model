10ffbf5af441b7130fe4db1b6d6fd614ddad9fc2
Gaetan <gaetan.snl@gmail.com>
2016-04-06 13:43:14 +0200

    Update main

--- main.c
+++ main.c
@@ -4,28 +4,88 @@
 #include "Table.h"
 #include <stdlib.h>
 #include <time.h>
+#include "io.h"
+#include "Utils.h"
+
+#include "MazeGeneration.h"
 
 #include "Neighborhood.h"
 
+/*
+	Gestion de la console avec compatibilite win32
+*/
+#ifdef WIN32
+void clearConsole() {
+	system("cls");
+}
+#else
+void clearConsole() {
+	system("clear");
+}
+#endif
+
 int current = 1;
-void transition(domainCellValue cell, Neighborhood *nbd) {
+TransitionsFunction transition(domainCellValue cell, Neighborhood *nbd) {
 	return current;
 }
 
-void control(Domain *domain, TransitionsExecControl *control) {
+/*
+Obtient ou definit le debug entre les etapes de l'epoch
+*/
+int epochStep = 0;
+
+/*
+Obtient ou definit le debug entre les generations
+*/
+int timeStepStep = 1;
+
+/*
+Obtient ou definit si le debug est interrompu jusqu'a la fin de l'epoch
+*/
+int skipEpochStep = 0;
+
+charTransformer charTransform(int c) {
+	switch (c)
+	{
+	case 0:
+		return ' ';
+	case 1:
+		return '*';
+	case 2:
+		return 'L';
+	case 3:
+		return 'R';
+	case 4:
+		return 'T';
+	case 5:
+		return 'B';
+	default:
+		return c + 48;
+		break;
+	}
+}
+
+/*
+	Controle du debug entre les transitions
+*/
+TransitionsControlFunction control(Domain *domain, TransitionsExecControl *control) {
+	skipEpochStep = 0;
+	if (!timeStepStep) {
+		return;
+	}
+
 	current++;
-	system(clear);
-	table_print_int(domain->array, domain->height, domain->width, 2);
-	printf("Epoch %d\n", control->timeStep);
+
+	clearConsole();
+	table_print_with_params(domain->array, table_print_char_line, domain->height, domain->width, 1, 1, charTransform);
+
+	printf("Time step %d\n", control->timeStep);
 	printf("******************************************************\n");
 	printf("*                      STATE CONTROL                 *\n");
-	printf("* (ENTREE) RUNNING - (a) ABORTED - (e) FINISHED *\n");
+	printf("*    (ENTREE) RUNNING - (a) ABORT - (e) FINISHED     *\n");
 	printf("******************************************************\n");
 
-	char c;
-	c = getchar();
-
-	switch (c)
+	switch (getOneChar())
 	{
 		case 'a':
 			control->state = ABORTED;
@@ -33,52 +93,120 @@ void control(Domain *domain, TransitionsExecControl *control) {
 		case 'e':
 			control->state = FINISHED;
 			break;
-	default:
-		break;
+		default:
+			break;
 	}
 }
 
+/*
+	Controle du debug pendant l'epoch
+*/
+TransitionsEpochWatch epoch(Domain *domain, TransitionsEpochProgression epochProgression) {
+	if (!epochStep || skipEpochStep) {
+		return;
+	}
+
+	clearConsole();
+	table_print_with_params(domain->array, table_print_char_line, domain->height, domain->width, 1, 1, charTransform);
+
+	printf("Epoch Progression %d/%d\n", epochProgression.current, epochProgression.maximum);
+	printf("Epoch %d \n", epochProgression.epoch);
+	printf("(ENTER) Continuer - (n) Time Step suivant\n");
+	skipEpochStep = getOneChar() == 'n';
+}
+
+void result(TransitionsExecResult result) {
+	printf("Resultat de l'execution \n");
+	printf("State : %d \n", result.state);
+	printf("Time step : %d \n", result.timeStep);
+	printf("Change : %d \n", result.change);
+}
+
+/*
+	Start et configuration
+*/
+void start() {
+	printf("Recherche de sortie d'un labyrinthe \n");
+	printf("----------------------------------- \n");
+	printf("CONFIGURATION : (n pour les resultats) \n");
+	printf("Voulez-vous faire des pauses entre les time Step ? y/n\n");
+	timeStepStep = yesno();
+	printf("Voulez-vous faire des pauses pour visualiser la progression de l'epoch ? y/n\n");
+	epochStep = yesno();
+}
+
+//TransitionsFunction mazeSolve(domainCellValue cell, Neighborhood *neighborhood) {
+//	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
+//
+//	int emptyCount = nbdValueCount(neighborhood, 0);
+//	int wallCount = nbdValueCount(neighborhood, 1);
+//	int undefinedCount = nbdUndefinedCount(neighborhood);
+//	int inaccessibleCount = undefinedCount + wallCount;
+//	int max = nbdValueGetMax(neighborhood);
+//
+//	if (cell.value == 8 && emptyCount == 1) {
+//		return 3;
+//	}
+//	
+//	if (!cell.value && nbdValueCount(neighborhood, 10) == 1 && !emptyCount && inaccessibleCount == 2) {
+//		return  mazePositionToDirBasic(nbdGetMaskByValue(neighborhood, 10));
+//	}
+//
+//	if (max >= 2 && max <= 5 && cell.value == 0 && emptyCount == 1 && inaccessibleCount == 2) {
+//		return mazePositionToDirBasic(nbdGetMaskByValue(neighborhood, max));
+//	}
+//
+//	if (!cell.value && inaccessibleCount == 3) {
+//		return 1;
+//	}
+//
+//	return cell.value;
+//}
+
+TransitionsFunction mazeSolve(domainCellValue cell, Neighborhood *neighborhood) {
+	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
+
+	int emptyCount = nbdValueCount(neighborhood, 0);
+	int close = nbdValueCount(neighborhood, 8);
+
+	if (cell.value == 0 && emptyCount == 0 && close) {
+		return mazePositionToDirBasic(nbdGetMaskByValue(neighborhood, 8));
+	}
+
+	if (cell.value == 0 && emptyCount == 1 && !close) {
+		return mazePositionToDirBasic(nbdGetMaskByValue(neighborhood, 0));
+	}
+
+	return cell.value;
+}
+
 int main() {
-	int table[4][4] = {
-		{1,2,3,4},
-		{ 1,2,3,4 },
-		{ 1,2,3,4 },
-		{ 1,2,3,4 }
-	};
+	start();
 
-	Domain *a = domainCreate(10, 10);
-	Domain *b = domainCopy(a);
-	TransitionsConfig config = {1};
-	te_run(a, config, transition, control);
-	//domainCellCoord c = { 4,4 };
-	//Neighborhood *n = createNeighborhood(a, c, 10);
+	TransitionsConfig config = { nbd_VON_NEUMANN };
 
+	Domain *a = mazeCreate(11,11);
+	while (1)
+	{
+		clearConsole();
+		Domain *a = mazeCreate(11, 11);
 
-//	table_print_int(table, 4, 4, 2);
-//	getchar();
-//	int b = TOP | RIGHT;
-//	
-//	te_run(a, transition, NULL);
-//
-//
-//	domainCellCoord c = {9, 9};
-//	setDomainCell(c, 100, a);
-////	Cell b = getDomainCell(c, a);
-//	//*(b.value) = 10;
-//	//int aaaa = *(b.value);
-//	int x;
-//	freeDomain(a);
-//	getchar(&x);
-//	//int table[2][2] = { {11,21},{31,41} };
-//	
-//	
-//	print_top(10, 6);
-//	print_line(10, 6);
-//	print_separator(10, 6);
-//	print_line(10, 6);
-//	print_bottom(10, 6);
-//	//call(test);
-//	//printf("%c",180 );
-//	
-//	getchar(&x);
+		domainCellCoord entrance = { 0, 0 };
+		domainCellCoord exit = { 10, 10 };
+
+		mazeSetPoints(a, entrance, exit, 0, 8);
+
+		table_print_with_params(a->array, table_print_char_line, a->height, a->width, 1, 1, charTransform);
+
+		TransitionsExecResult results = te_run(a, config, mazeSolve, control, epoch);
+
+		table_print_with_params(a->array, table_print_char_line, a->height, a->width, 1,1, charTransform);		 
+		result(results);
+
+		domainFree(a);
+		getchar();	
+	}
+
+	//TransitionsConfig config = {1};
+	//te_run(a, config, transition, control, epoch);
 }
