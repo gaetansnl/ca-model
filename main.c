e00a18915e98bc8341088b8511b532906e3a42ef
Gaetan <gaetan.snl@gmail.com>
2016-05-09 15:21:58 +0200

    Add all transition functions main

--- main.c
+++ main.c
@@ -50,7 +50,7 @@ charTransformer charTransform(int c) {
 	case 0:
 		return ' ';
 	case 1:
-		return '*';
+		return 219;
 	case 2:
 		return 'L';
 	case 3:
@@ -91,11 +91,25 @@ TransitionsControlFunction control(Domain *domain, TransitionsExecControl *contr
 			control->state = ABORTED;
 			break;
 		case 'e':
-			control->state = FINISHED;
+			control->state = CONTROL_FINISHED;
 			break;
 		default:
 			break;
 	}
+}
+
+TransitionsControlFunction mazeCtrl(Domain *domain, TransitionsExecControl *transitionControl) {
+	domainCellCoord c = { 0,0 };
+	domainCellType dir[4] = {maze_BOTTOM, maze_LEFT, maze_RIGHT, maze_TOP};
+	Neighborhood* nbd = createNeighborhoodAndInit(domain, c, nbd_VON_NEUMANN);
+
+	if (domainGetCellValue(c, domain).value && nbdGetMaskByValues(nbd, dir, 4, 1)) {
+		transitionControl->state = CONTROL_FINISHED;
+	}else{
+		control(domain, transitionControl);
+	}
+
+	freeNeighborhood(nbd);
 }
 
 /*
@@ -135,35 +149,10 @@ void start() {
 	epochStep = yesno();
 }
 
-//TransitionsFunction mazeSolve(domainCellValue cell, Neighborhood *neighborhood) {
-//	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
-//
-//	int emptyCount = nbdValueCount(neighborhood, 0);
-//	int wallCount = nbdValueCount(neighborhood, 1);
-//	int undefinedCount = nbdUndefinedCount(neighborhood);
-//	int inaccessibleCount = undefinedCount + wallCount;
-//	int max = nbdValueGetMax(neighborhood);
-//
-//	if (cell.value == 8 && emptyCount == 1) {
-//		return 3;
-//	}
-//	
-//	if (!cell.value && nbdValueCount(neighborhood, 10) == 1 && !emptyCount && inaccessibleCount == 2) {
-//		return  mazePositionToDirBasic(nbdGetMaskByValue(neighborhood, 10));
-//	}
-//
-//	if (max >= 2 && max <= 5 && cell.value == 0 && emptyCount == 1 && inaccessibleCount == 2) {
-//		return mazePositionToDirBasic(nbdGetMaskByValue(neighborhood, max));
-//	}
-//
-//	if (!cell.value && inaccessibleCount == 3) {
-//		return 1;
-//	}
-//
-//	return cell.value;
-//}
-
-TransitionsFunction mazeSolve(domainCellValue cell, Neighborhood *neighborhood) {
+/*
+	Résolution avec la solution B
+*/
+TransitionsFunction mazeSolveAll(domainCellValue cell, Neighborhood *neighborhood) {
 	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
 
 	int emptyCount = nbdValueCount(neighborhood, 0);
@@ -180,30 +169,166 @@ TransitionsFunction mazeSolve(domainCellValue cell, Neighborhood *neighborhood) 
 	return cell.value;
 }
 
+/*
+Résolution avec la solution la solution A
+*/
+TransitionsFunction mazeSolveSimple(domainCellValue cell, Neighborhood *neighborhood) {
+	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
+
+	int wallCount = nbdValueCount(neighborhood, 1) + nbdUndefinedCount(neighborhood);
+
+	if (cell.value == 0 && wallCount == 3) {
+		return 1;
+	}
+
+	return cell.value;
+}
+
+
+//Necessite une entrée définie
+/*
+	Résolution avec la solution générale et la solution A
+*/
+TransitionsFunction mazeSolveAndSimple(domainCellValue cell, Neighborhood *neighborhood) {
+	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
+
+	domainCellType dirCells[5] = {maze_BOTTOM, maze_LEFT, maze_RIGHT, maze_TOP, 8};
+
+	int wallCount = nbdValueCount(neighborhood, 1) + nbdUndefinedCount(neighborhood);
+
+	if (cell.value == 0 && wallCount == 3) {
+		return 1;
+	}
+
+	int dirCount = nbdGetMaskByValues(neighborhood, &dirCells, 5, 1);
+	if (cell.value == 0 && dirCount) {
+		return mazePositionToDirBasic(dirCount);
+	}
+
+	return cell.value;
+}
+
+/*
+	Résolution du labyrinthe avec la méthode générale
+*/
+TransitionsFunction mazeSolve(domainCellValue cell, Neighborhood *neighborhood) {
+	invariant(nbd_VON_NEUMANN, neighborhood->mask, "Invalid mask");
+
+	domainCellType dirCells[5] = { maze_BOTTOM, maze_LEFT, maze_RIGHT, maze_TOP, 8 };
+
+	int dirCount = nbdGetMaskByValues(neighborhood, &dirCells, 5, 1);
+	if (cell.value == 0 && dirCount) {
+		return mazePositionToDirBasic(dirCount);
+	}
+
+	return cell.value;
+}
+
+/*
+	Génération d'un labyrinthe avec la règle B3 S12345 
+	Les cellules prennent la valeur 1 si elles ont trois voisins
+	Gardent la valeur 1 si 1,2,3,4,5 voisins
+	Prennent la valeur 0 sinon
+*/
+TransitionsFunction b3s12345(domainCellValue cell, Neighborhood *neighborhood) {
+	invariant(nbd_MOORE, neighborhood->mask, "Invalid mask");
+
+	int count = nbdValueCount(neighborhood, 1);
+
+	if (cell.value == 0 && count == 3) {
+		return 1;
+	}
+
+	if (cell.value == 1 && count >= 1 && count <= 5) {
+		return 1;
+	}
+
+	return 0;
+}
+
+Domain* getSpecialMaze(){
+	Domain *a = domainCreate(11, 11);
+
+	a->array[5][0] = 1;
+	a->array[5][1] = 1;
+	a->array[5][2] = 1;
+	a->array[5][3] = 1;
+	a->array[5][4] = 1;
+	a->array[6][4] = 1;
+	a->array[7][4] = 1;
+	a->array[8][4] = 1;
+	a->array[9][4] = 1;
+
+	a->array[0][5] = 1;
+	a->array[1][5] = 1;
+	a->array[2][5] = 1;
+	a->array[3][5] = 1;
+
+
+	a->array[0][7] = 1;
+	a->array[1][7] = 1;
+	a->array[2][7] = 1;
+	a->array[3][7] = 1;
+	a->array[4][7] = 1;
+	a->array[5][7] = 1;
+	a->array[6][7] = 1;
+
+	return a;
+}
+
 int main() {
 	start();
 
 	TransitionsConfig config = { nbd_VON_NEUMANN };
 
-	Domain *a = mazeCreate(11,11);
 	while (1)
 	{
-		clearConsole();
-		Domain *a = mazeCreate(11, 11);
+		Domain *DomainB = mazeCreate(11, 11);
+		Domain *DomainSolve = domainCopy(DomainB);
+		Domain *DomainSolve2 = domainCopy(DomainB);
+
+		Domain *DomainA = domainCopy(DomainB);
+		Domain *DomainSolveAndSimple = domainCopy(DomainB);
 
 		domainCellCoord entrance = { 0, 0 };
 		domainCellCoord exit = { 10, 10 };
 
-		mazeSetPoints(a, entrance, exit, 0, 8);
+		mazeSetPoints(DomainB, entrance, exit, 0, 8);
+		mazeSetPoints(DomainSolve, entrance, exit, 0, 8);
+		mazeSetPoints(DomainSolve2, entrance, exit, 0, 8);
 
-		table_print_with_params(a->array, table_print_char_line, a->height, a->width, 1, 1, charTransform);
+		mazeSetPoints(DomainA, entrance, exit, 9, 8);
+		mazeSetPoints(DomainSolveAndSimple, entrance, exit, 9, 8);
 
-		TransitionsExecResult results = te_run(a, config, mazeSolve, control, epoch);
+		clearConsole();
 
-		table_print_with_params(a->array, table_print_char_line, a->height, a->width, 1,1, charTransform);		 
+		TransitionsExecResult results;
+		results = te_run(DomainA, config, mazeSolveSimple, control, epoch);
+		table_print_with_params(DomainA->array, table_print_char_line, DomainA->height, DomainA->width, 1, 1, charTransform);
 		result(results);
 
-		domainFree(a);
+		results = te_run(DomainB, config, mazeSolveAll, control, epoch);
+		table_print_with_params(DomainB->array, table_print_char_line, DomainB->height, DomainB->width, 1, 1, charTransform);
+		result(results);
+
+		results = te_run(DomainSolve, config, mazeSolve, control, epoch);
+		table_print_with_params(DomainSolve->array, table_print_char_line, DomainSolve->height, DomainSolve->width, 1, 1, charTransform);
+		result(results);
+
+		results = te_run(DomainSolve2, config, mazeSolve, mazeCtrl, epoch);
+		table_print_with_params(DomainSolve2->array, table_print_char_line, DomainSolve2->height, DomainSolve2->width, 1, 1, charTransform);
+		result(results);
+
+		results = te_run(DomainSolveAndSimple, config, mazeSolveAndSimple, control, epoch);
+		table_print_with_params(DomainSolveAndSimple->array, table_print_char_line, DomainSolveAndSimple->height, DomainSolveAndSimple->width, 1, 1, charTransform);
+		result(results);
+
+		domainFree(DomainA);
+		domainFree(DomainB);
+		domainFree(DomainSolve);
+		domainFree(DomainSolve2);
+		domainFree(DomainSolveAndSimple);
+
 		getchar();	
 	}
 
