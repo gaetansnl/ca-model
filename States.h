a631a9d5e3de1060c09547b106b9271cab210105
Gaetan <gaetan.snl@gmail.com>
2016-05-10 17:55:48 +0200

    Comment states

--- States.h
+++ States.h
@@ -1,11 +1,11 @@
 #pragma once
 
-enum States {
-	TARGET = -1,
-	EMPTY = 0,
-	WALL = 1,
-	LEFT = 2,
-	RIGHT = 3,
-	TOP = 4,
-	BOTTOM = 3
-};
+//enum States {
+//	TARGET = -1,
+//	EMPTY = 0,
+//	WALL = 1,
+//	LEFT = 2,
+//	RIGHT = 3,
+//	TOP = 4,
+//	BOTTOM = 3
+//};
